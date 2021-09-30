f0604eb3b59f01ee314f01abb804895752489edf
Eric938 <89991891544@yandex.ru>
2021-09-30 14:46:45 +0400

    Исправления

--- js/main.js
+++ js/main.js
@@ -1,32 +1,15 @@
-function getRandomInt(min, max) {
-  if (min<0) {
-    min = min*-1;
-  }
-
-  if (max<0) {
-    max = max*-1;
-  }
-
-  if (min>=max) {
-    max = 2*min;
-  }
-
-  //Получение случайного целого числа в заданном интервале я взял с сайта https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-
+const getRandomInteger = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
-
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-getRandomInt(0, 100);
-
-function lengthСheck(symbols, limit) {
-  if (symbols>limit) {
-    return false;
+  if (min < 0 || max < 0 || min > max) {
+    throw new RangeError('Неправильно задан диапазон');
   }
 
-  return true;
-}
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
 
-lengthСheck(100, 140);
+getRandomInteger(10,100);
+
+const checkLength = (comment, limit) => comment.length <= limit;
+
+checkLength('Надеюсь,я правильно исправил все косяки.Кстати,спасибо за исправления', 140);
